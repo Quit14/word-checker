65960124f9cc44039de0607f5202e29137cbb631
Anastasiya Volkova <ahh.write@gmail.com>
2022-08-20 11:03:22 +0300

    v. 1.1, rework 08-20-2022

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
         WordsChecker loremIpsum = new WordsChecker(TEXT);
-        System.out.println(loremIpsum.hasWord("sed"));
+        System.out.println(loremIpsum.hasWord("ut"));
     }
 }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -3,20 +3,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class WordsChecker {
-    private String text;
+    private Set<String> words;
 
     public WordsChecker(String text) {
-        this.text = text;
+        String[] values = text.split("\\P{IsAlphabetic}+");
+        this.words = new HashSet<>(Arrays.asList(values));
     }
 
     public boolean hasWord(String word) {
-        String[] values = text.split("\\P{IsAlphabetic}+");
-        Set<String> words = new HashSet<>(Arrays.asList(values));
-        if (words.contains(word)) {
-            return true;
-        } else {
-            return false;
-        }
+        return words.contains(word);
 
     }
+
+
 }
+
